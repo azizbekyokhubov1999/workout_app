4b6e5b32b3e7edc30a246840b16801fd52d50a03
Azizbek <azizbek.yoqubov.99@gmail.com>
2024-07-31 16:08:34 +0300

    workout app is completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,6 +10,7 @@ class HomePage extends StatefulWidget {
   @override
   State<HomePage> createState() => _HomePageState();
   // just for check
+  // just for check
 }
 
 class _HomePageState extends State<HomePage> {
